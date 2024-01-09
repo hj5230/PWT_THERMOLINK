eb406852b12f52874f66dd4d96b2105345f32627
hj5230 <hf305@foxmail.com>
2024-01-09 16:16:38 +0200

    add login GUI

--- src/renderer/src/components/Login.tsx
+++ src/renderer/src/components/Login.tsx
@@ -1,22 +1,57 @@
-import { Button, Modal } from 'antd'
 import React from 'react'
+import { Modal, Input, Button } from 'antd'
+import style from '@renderer/assets/login.module.less'
 
 interface Props {
   isLoginOpen: boolean
   onClose: () => void
 }
 
-class Login extends React.Component<Props, object> {
+interface State {
+  inputsStatus: boolean[]
+  loading: boolean
+}
+
+class Login extends React.Component<Props, State> {
+  state: Readonly<State> = {
+    inputsStatus: [true, true, true],
+    loading: false
+  }
+
+  onSubmit = (): void => {
+    this.setState({ loading: true })
+  }
+
   render(): React.ReactNode {
+    const { onSubmit } = this
     const { isLoginOpen, onClose } = this.props
+    const { inputsStatus, loading } = this.state
     return (
       <Modal
         title="Login"
-        okText="Continue"
         open={isLoginOpen}
         onCancel={onClose}
+        footer={
+          <Button type="primary" loading={loading} onClick={onSubmit}>
+            Submit
+          </Button>
+        }
       >
-        <Button>submit</Button>
+        <Input
+          className={style.field}
+          placeholder="Product ID"
+          status={inputsStatus[0] ? '' : 'error'}
+        />
+        <Input
+          className={style.field}
+          placeholder="Username"
+          status={inputsStatus[1] ? '' : 'error'}
+        />
+        <Input
+          className={style.field}
+          placeholder="Password"
+          status={inputsStatus[2] ? '' : 'error'}
+        />
       </Modal>
     )
   }
