30dc34f5e2a04773c0457fa477e20fc5f8aeb829
hj5230 <hf305@foxmail.com>
2024-01-21 01:47:46 +0200

    remove useless component

--- src/renderer/src/components/Actbtns.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import style from '@renderer/assets/less/actbar.module.less'
-import { Button } from 'antd'
-
-const LeftBtn: React.FC = (): React.ReactNode => <Button>Left</Button>
-
-const RightBtn: React.FC = (): React.ReactNode => <Button>Right</Button>
-
-export { LeftBtn, RightBtn }
