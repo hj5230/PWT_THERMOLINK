08a15f9dc6c0165251706f5b1578f509e2c0905a
hj5230 <hf305@foxmail.com>
2024-02-05 18:46:40 +0200

    add button icon

--- src/renderer/src/components/Actionbar.tsx
+++ src/renderer/src/components/Actionbar.tsx
@@ -1,21 +1,33 @@
 import React from 'react'
 import Voice from './Voice'
 import { Row, Col, Button, Flex } from 'antd'
+import { AreaChartOutlined, ControlOutlined } from '@ant-design/icons'
 
 interface Props {
   windowWidth: number
   widgetHeight: number
 }
 
-class Actionbar extends React.Component<Props, object> {
-  getVoiceBtnSize = (): number => {
+interface State {
+  generalizedSize: number
+}
+
+class Actionbar extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      generalizedSize: this.getGeneralizedSize()
+    }
+  }
+
+  getGeneralizedSize = (): number => {
     const { windowWidth } = this.props
     return (windowWidth - 16) / 6
   }
 
   render(): React.ReactNode {
-    const { getVoiceBtnSize } = this
     const { windowWidth } = this.props
+    const { generalizedSize } = this.state
     return (
       <>
         <Row
@@ -27,18 +39,22 @@ class Actionbar extends React.Component<Props, object> {
           }}
         >
           <Col span={10}>
-            <Flex justify="flex-start" align="flex-end" style={{ height: getVoiceBtnSize() }}>
-              <Button type="primary" style={{ width: '100%', height: '50%' }}></Button>
+            <Flex justify="flex-start" align="flex-end" style={{ height: generalizedSize }}>
+              <Button type="primary" style={{ width: '100%', height: '50%' }}>
+                <AreaChartOutlined style={{ fontSize: 32 }} />
+              </Button>
             </Flex>
           </Col>
           <Col span={4}>
             <Flex justify="center" align="center">
-              <Voice buttonSize={getVoiceBtnSize()} />
+              <Voice buttonSize={generalizedSize} />
             </Flex>
           </Col>
           <Col span={10}>
-            <Flex justify="flex-end" align="flex-end" style={{ height: getVoiceBtnSize() }}>
-              <Button type="primary" style={{ width: '100%', height: '50%' }}></Button>
+            <Flex justify="flex-end" align="flex-end" style={{ height: generalizedSize }}>
+              <Button type="primary" style={{ width: '100%', height: '50%' }}>
+                <ControlOutlined style={{ fontSize: 32 }} />
+              </Button>
             </Flex>
           </Col>
         </Row>
