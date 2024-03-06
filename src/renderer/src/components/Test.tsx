5f5e489ac5e9f397e897371a67a98e55053739cc
kongbai23330 <109857471+kongbai23330@users.noreply.github.com>
2024-03-06 17:39:30 +0200

    Add files via upload

--- src/renderer/src/components/Test.tsx
+++ src/renderer/src/components/Test.tsx
@@ -1,15 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { notification } from 'antd';
 
 class Test extends React.Component {
   componentDidMount = async (): Promise<void> => {
-    // fetch(`http://localhost:5000/`)
-    //   .then((e) => e.json())
-    //   .then((e) => console.log(e))
-  }
+    const payload = {
+      features_heater_on_time: [0, 18, 5, 50],
+      features_temp: [0, 18],
+      features_heating_time: [19, 5, 50],
+    };
+
+    fetch(`http://localhost:5000/predict`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Prediction Results:', data); // Log the data to console
+        this.openNotificationWithIcon('info', data);
+      })
+      .catch((error) => {
+        console.error('There was a problem with your fetch operation:', error);
+        this.openNotificationWithIcon('error', { message: 'Failed to fetch', description: error.message });
+      });
+  };
+
+  openNotificationWithIcon = (type, data) => {
+    notification[type]({
+      message: 'Prediction Results',
+      description: (
+        <div>
+          <p>Heater On Time Prediction: {data.heater_on_time_prediction}</p>
+          <p>Target Temperature Prediction: {data.target_temperature_prediction}</p>
+          <p>Heating Time Prediction: {data.heating_time_prediction}</p>
+        </div>
+      ),
+      duration: 0, // set duration to 0 to make the notification stay until closed manually
+    });
+  };
 
   render(): React.ReactNode {
-    return <></>
+    return <></>;
   }
 }
 
-export default Test
+export default Test;
