6e4a85b3e1653c1c03c0dc59e5218d90fa353208
hj5230 <hf305@foxmail.com>
2024-03-06 17:08:34 +0200

    add login

--- src/renderer/src/components/Login.tsx
+++ src/renderer/src/components/Login.tsx
@@ -8,24 +8,51 @@ interface Props {
 }
 
 interface State {
+  loginError: string | null
   inputsStatus: boolean[]
   loading: boolean
+  productId: string
+  username: string
+  email: string
 }
 
 class Login extends React.Component<Props, State> {
   state: Readonly<State> = {
+    loginError: null,
     inputsStatus: [true, true, true],
-    loading: false
+    loading: false,
+    productId: '',
+    username: '',
+    email: ''
   }
 
-  onSubmit = (): void => {
+  handleInputChange = (field: keyof State, value: string): void => {
+    this.setState({ [field]: value } as unknown as Pick<State, keyof State>)
+  }
+
+  onSubmit = async (): Promise<void> => {
+    const { productId, username, email } = this.state
     this.setState({ loading: true })
+    const response = await fetch(`http://localhost:5000/sign`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        productId,
+        username,
+        email
+      })
+    })
+    const result = await response.json()
+    if (result.msg || !result.jwt) this.setState({ loginError: result.msg })
+    localStorage.setItem('jwt', result.jwt)
   }
 
   render(): React.ReactNode {
     const { onSubmit } = this
     const { isLoginOpen, onClose } = this.props
-    const { inputsStatus, loading } = this.state
+    const { inputsStatus, loading, productId, username, email } = this.state
     return (
       <Modal
         title="Login"
@@ -41,16 +68,22 @@ class Login extends React.Component<Props, State> {
           className={style.field}
           placeholder="Product ID"
           status={inputsStatus[0] ? '' : 'error'}
+          value={productId}
+          onChange={(e) => this.handleInputChange('productId', e.target.value)}
         />
         <Input
           className={style.field}
           placeholder="Username"
           status={inputsStatus[1] ? '' : 'error'}
+          value={username}
+          onChange={(e) => this.handleInputChange('username', e.target.value)}
         />
         <Input
           className={style.field}
-          placeholder="Password"
+          placeholder="Email"
           status={inputsStatus[2] ? '' : 'error'}
+          value={email}
+          onChange={(e) => this.handleInputChange('email', e.target.value)}
         />
       </Modal>
     )
