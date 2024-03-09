4b0426a54d0233989b2f3cdaa8ee6b467e3bd7fc
hj5230 <hf305@foxmail.com>
2024-03-09 16:10:33 +0200

    set notification autoclose

--- src/renderer/src/components/Test.tsx
+++ src/renderer/src/components/Test.tsx
@@ -1,36 +1,39 @@
-import React from 'react';
-import { notification } from 'antd';
+import React from 'react'
+import { notification } from 'antd'
 
 class Test extends React.Component {
   componentDidMount = async (): Promise<void> => {
     const payload = {
       features_heater_on_time: [0, 18, 5, 50],
       features_temp: [0, 18],
-      features_heating_time: [19, 5, 50],
-    };
+      features_heating_time: [19, 5, 50]
+    }
 
     fetch(`http://localhost:5000/predict`, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(payload),
+      body: JSON.stringify(payload)
     })
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok')
         }
-        return response.json();
+        return response.json()
       })
       .then((data) => {
-        console.log('Prediction Results:', data); // Log the data to console
-        this.openNotificationWithIcon('info', data);
+        console.log('Prediction Results:', data) // Log the data to console
+        this.openNotificationWithIcon('info', data)
       })
       .catch((error) => {
-        console.error('There was a problem with your fetch operation:', error);
-        this.openNotificationWithIcon('error', { message: 'Failed to fetch', description: error.message });
-      });
-  };
+        console.error('There was a problem with your fetch operation:', error)
+        this.openNotificationWithIcon('error', {
+          message: 'Failed to fetch',
+          description: error.message
+        })
+      })
+  }
 
   openNotificationWithIcon = (type, data) => {
     notification[type]({
@@ -41,14 +44,13 @@ class Test extends React.Component {
           <p>Target Temperature Prediction: {data.target_temperature_prediction}</p>
           <p>Heating Time Prediction: {data.heating_time_prediction}</p>
         </div>
-      ),
-      duration: 0, // set duration to 0 to make the notification stay until closed manually
-    });
-  };
+      )
+    })
+  }
 
   render(): React.ReactNode {
-    return <></>;
+    return <></>
   }
 }
 
-export default Test;
+export default Test
