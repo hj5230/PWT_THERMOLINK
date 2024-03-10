c0507c5101a818ade1d238170496aba1bc887919
hj5230 <hf305@foxmail.com>
2024-03-10 16:02:04 +0200

    update control UI

--- src/renderer/src/components/Control.tsx
+++ src/renderer/src/components/Control.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
-import { Card, Row, Col, Select, Switch, Slider, TimePicker, Button, Flex } from 'antd'
+import {
+  Card,
+  Flex,
+  Row,
+  Col,
+  Select,
+  Switch,
+  Slider,
+  TimePicker,
+  Button,
+  Tag,
+  InputNumber
+} from 'antd'
 import { ClockCircleOutlined, CloseOutlined } from '@ant-design/icons'
+import style from '@renderer/assets/less/viewport.module.less'
 
 const { Option } = Select
 
@@ -8,9 +21,44 @@ interface Props {
   back: () => void
 }
 
-class Control extends React.Component<Props, object> {
+interface State {
+  value: number[]
+}
+
+class Control extends React.Component<Props, State> {
+  state: State = {
+    value: [10, 20]
+  }
+
+  getGradientColor = (percentage: number): string => {
+    const startColor = [135, 208, 104] // Green-ish
+    const endColor = [255, 204, 199] // Red-ish
+    const midColor = startColor.map((start, i) => {
+      const end = endColor[i]
+      const delta = end - start
+      return Math.round(start + delta * percentage).toString()
+    })
+
+    return `rgb(${midColor.join(',')})`
+  }
+
+  setValue = (e: number[]): void => {
+    this.setState({ value: e })
+  }
+
+  setTemp = (e: number | null): void => {
+    const { value } = this.state
+    if (!e) return
+    value[1] = e
+    this.setState({ value: value })
+  }
+
   render(): React.ReactNode {
+    const { getGradientColor, setValue, setTemp } = this
     const { back } = this.props
+    const { value } = this.state
+    const start = value[0] / 100
+    const end = value[value.length - 1] / 100
     return (
       <>
         <Card>
@@ -23,32 +71,37 @@ class Control extends React.Component<Props, object> {
               onClick={back}
             />
           </Flex>
+          <Row className={style.row}>
+            <Tag color="cyan">Smart Control Mode</Tag>
+            <Switch checkedChildren="ON" unCheckedChildren="OFF" />
+          </Row>
+          <Row className={style.row}>
+            <Tag color="red">Power</Tag>
+            <Switch checkedChildren="ON" unCheckedChildren="OFF" />
+          </Row>
           <Row>
-            <Col span={10}>Power</Col>
+            <Col span={11}>
+              <Tag className={style.row} color="green">
+                Mode
+              </Tag>
+            </Col>
             <Col span={2} />
-            <Col span={20}>
-              <Switch checkedChildren="ON" unCheckedChildren="OFF" />
+            <Col span={11}>
+              <Tag className={style.row} color="geekblue">
+                Schedule
+              </Tag>
             </Col>
           </Row>
-          <Row gutter={16} style={{ marginBottom: '20px' }}>
-            <Col span={12}>
-              <p>Mode</p>
-              <Select style={{ width: '100%' }}>
+          <Row>
+            <Col span={11}>
+              <Select style={{ width: '100%' }} placeholder="mode">
                 <Option value="eco">Eco</Option>
                 <Option value="comfort">Comfort</Option>
                 <Option value="antiFreeze">Anti-freeze</Option>
               </Select>
             </Col>
-          </Row>
-          <Row gutter={16} style={{ marginBottom: '20px' }}>
-            <Col span={24}>
-              <p>Temperature</p>
-              <Slider min={10} max={30} marks={{ 10: '10°C', 30: '30°C' }} />
-            </Col>
-          </Row>
-          <Row gutter={16} style={{ marginBottom: '20px' }}>
-            <Col span={24}>
-              <p>Schedule</p>
+            <Col span={2} />
+            <Col span={11}>
               <TimePicker
                 format="HH:mm"
                 placeholder="Startup Time"
@@ -59,6 +112,34 @@ class Control extends React.Component<Props, object> {
                 placeholder="Shutdown Time"
                 suffixIcon={<ClockCircleOutlined />}
               />
+              <Button type="primary">Update</Button>
+            </Col>
+          </Row>
+          <Tag className={style.row} color="gold">
+            Temperature
+          </Tag>
+          <Row>
+            <Col span={20}>
+              <Slider
+                range
+                min={10}
+                max={25}
+                defaultValue={value}
+                onChange={setValue}
+                styles={{
+                  track: {
+                    background: 'transparent'
+                  },
+                  rail: {
+                    background: `linear-gradient(to right, ${getGradientColor(
+                      start
+                    )} 0%, ${getGradientColor(end / 0.3)} 100%)`
+                  }
+                }}
+              />
+            </Col>
+            <Col span={4}>
+              <InputNumber value={value[1]} onChange={setTemp}></InputNumber>
             </Col>
           </Row>
         </Card>
